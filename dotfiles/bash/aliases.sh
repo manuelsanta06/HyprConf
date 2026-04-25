c899d114acc5ff8ebfbc83bc5243482486625510
santa <santarellihectorm@gmail.com>
2026-04-25 18:30:52 -0300

    bash rearangement

---
+++ dotfiles/bash/aliases.sh
@@ -0,0 +1,13 @@
+alias get_idf='. $HOME/clons/esp-idf/export.sh; alias idf="idf.py";alias build="time idf.py build"; alias fullclean="idf.py fullclean"'
+
+
+alias log="git log --all --decorate --oneline --graph"
+alias steamopen='xdg-open steam://rungameid/'
+alias grep='grep --color=auto'
+alias ls='ls --color=auto'
+alias cls='clear; fastfetch'
+alias please='sudo'
+alias pls='sudo'
+alias ln='ln -s'
+alias :q='exit'
+
