fbb69ca65119ee06b5f2abe87329415769aa97c2
santa <santarellihectorm@gmail.com>
2026-06-20 02:04:44 -0300

    rWallpapar improvement

--- scripts/rWallpaper.sh
+++ scripts/rWallpaper.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-WCOUNT="$(ls -1q ~/resources/wallpapers/ | wc -l)"
+WALLPAPERS_DIR="$HOME/resources/wallpapers"
 FOCUSED_OUTPUT=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
-[ ! -z $1 ] && 
-  WALLPAPER_DIR=$(ls ~/resources/wallpapers/$1.* | head -n 1) ||
-  WALLPAPER_DIR=$(ls ~/resources/wallpapers/$((1+$RANDOM%$WCOUNT)).* | head -n 1)
 
-awww img --transition-type outer --transition-pos 0.999,0.999 --transition-step 90 "$WALLPAPER_DIR" -o "$FOCUSED_OUTPUT"
+if [[ -n "$1" ]];then
+  WALLPAPER=$(find "$WALLPAPERS_DIR" -maxdepth 1 -type f -name "$1.*" | head -n 1)
+  
+  if [[ -z "$WALLPAPER" ]];then
+    echo "Error: No such file '$1'"
+    exit 1
+  fi
+else
+  WALLPAPER=$(find "$WALLPAPERS_DIR" -maxdepth 1 -type f | shuf -n 1)
+fi
+
+awww img --transition-type outer --transition-pos 0.999,0.999 --transition-step 90 "$WALLPAPER" -o "$FOCUSED_OUTPUT"
