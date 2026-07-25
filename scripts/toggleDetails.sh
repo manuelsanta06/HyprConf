b1103654f5766d8e0595e727638a2f32ef61728c
santa <santarellihectorm@gmail.com>
2026-07-25 20:08:09 -0300

    display Hz smart reduction

--- scripts/toggleDetails.sh
+++ scripts/toggleDetails.sh
@@ -3,19 +3,25 @@
 BLUR_STATE=$(hyprctl getoption decoration:blur:enabled -j | jq '.bool')
 
 if [ "$BLUR_STATE" = "true" ];then
-  hyprctl eval "hl.config({
-    decoration={
-      shadow={enabled=false},
-      blur={enabled=false},
-      rounding=0
-    },
-    general={
-      border_size=3,
-      gaps_in=1,
-      gaps_out=0
-    },
-    animations={enabled=false}
-  })">/dev/null 2>&1
+  hyprctl eval "
+    hl.config({
+      decoration={
+        shadow={enabled=false},
+        blur={enabled=false},
+        rounding=0
+      },
+      general={
+        border_size=3,
+        gaps_in=1,
+        gaps_out=0
+      },
+      animations={enabled=false}
+    })
+    hl.monitor({
+      output   = 'eDP-1',
+      mode     = '1920x1080@60'
+    })
+  " >/dev/null 2>&1
   echo "false"
 else
   hyprctl reload>/dev/null 2>&1
