55bf281a398fa2ae7bfc203d3cf99fa4844fb7f5
santa <santarellihectorm@gmail.com>
2026-08-28 02:15:44 -0300

    git log noew global git alias

--- dotfiles/bash/aliases.sh
+++ dotfiles/bash/aliases.sh
@@ -1,7 +1,6 @@
 alias get_idf='. $HOME/clons/esp-idf/export.sh; alias idf="idf.py";alias build="time idf.py build"; alias fullclean="idf.py fullclean"'
 
 
-alias log="git log --all --decorate --oneline --graph"
 alias steamopen='xdg-open steam://rungameid/'
 alias grep='grep --color=auto'
 alias ls='ls --color=auto'
